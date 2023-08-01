3e2b417cef27c2caf2ef219066b65a8825b8aaff
Dede Harianto <ddharianto11@gmail.com>
2023-08-01 10:12:42 +0700

    install deps

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="">
+      <h1 className="flex justify-center items-center text-3xl font-bold text-white underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
